5512451c67d10dc6688e62d6d7f2e6480bdae15f
Calvin <calvin.peak@gmail.com>
2016-06-23 16:16:27 +0900

    Add CHCubicBezier class in project.

---
+++ CHCubicBezier/CHCubicBezier.swift
@@ -0,0 +1,15 @@
+//
+//  CHCubicBezier.swift
+//  CHCubicBezier
+//
+//  Created by Calvin on 6/23/16.
+//  Copyright © 2016 CapsLock. All rights reserved.
+//
+
+//  Reference from https://github.com/gre/bezier-easing
+
+import Foundation
+
+class CHCubicBezier {
+    
+}
