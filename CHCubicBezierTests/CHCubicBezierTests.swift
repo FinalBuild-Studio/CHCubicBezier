c4ad4c13a326b1044a4f301498c6295a60b23ef9
Calvin <calvin.peak@gmail.com>
2016-06-23 19:45:52 +0900

    Add Unit Test.

--- CHCubicBezierTests/CHCubicBezierTests.swift
+++ CHCubicBezierTests/CHCubicBezierTests.swift
@@ -10,27 +10,10 @@ import XCTest
 @testable import CHCubicBezier
 
 class CHCubicBezierTests: XCTestCase {
-    
-    override func setUp() {
-        super.setUp()
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+    func testCubicBezier() {
+        let cubicBezier = CubicBezier(mX1: 0, mY1: 0, mX2: 1, mY2: 0.5)
+        XCTAssert(cubicBezier.easing(0.0) == 0.0)
+        XCTAssert(cubicBezier.easing(0.5) == 0.3125, "Value: \(cubicBezier.easing(0.5)) should equals to 0.3125")
+        XCTAssert(cubicBezier.easing(1.0) == 1.0)
     }
-    
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-        super.tearDown()
-    }
-    
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-    
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measureBlock {
-            // Put the code you want to measure the time of here.
-        }
-    }
-    
 }
